c3fc936fe39983a3346881da3ce729d8c1a15074
Uknown_player <evgeniiledin@gmail.com>
2018-04-01 20:07:31 +0200

    refactoring

--- Set/Card.swift
+++ Set/Card.swift
@@ -9,23 +9,20 @@
 import UIKit
 
 struct Card: Equatable {
-  
-  static func ==(lhs: Card, rhs: Card) -> Bool {
-    return lhs.color == rhs.color && lhs.shape == rhs.shape &&
-      lhs.number == rhs.number && lhs.shading == rhs.shading
-  }
-  
-  
   let color: UIColor
   let shape: Shape
   let number: Number
   let shading: Shading
-  
   
   init(put shape: Shape, times number: Number, apply shading: Shading, paint color: UIColor) {
     self.shape = shape
     self.number = number
     self.shading = shading
     self.color = color
+  }
+  
+  static func ==(lhs: Card, rhs: Card) -> Bool {
+    return lhs.color == rhs.color && lhs.shape == rhs.shape &&
+      lhs.number == rhs.number && lhs.shading == rhs.shading
   }
 }
